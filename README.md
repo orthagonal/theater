30cadda24fb91b4d84dab2410001ead2d59284e5
chris <orthagonal1@gmail.com>
2016-07-24 23:52:47 -0500

    updated some lint issues
    updated some lint things

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 
 
 # Theater Interactive Video Engine  
-## an engine for making interactive full-motion video games
+## an nwjs-based engine for making interactive full-motion video games
 
+installation:
+```
+npm install theater
+```
 
 A 'module' consists of:
 - a nested directory structure of video clips
-- a Room Graph, which tells Theater how the film clips go together
--
+- a Room Map or Graph, which describes the assets/film clips used in the 'rooms' and how they all relate to each other
+- a Directory, script which tells Theater how to process queries and switch among the clips within each 'room'
+- a directory of images
+- one or more ClientHandlers (a list of event handlers for user-input events)
+
+  A 'room' is a grouping of film clips, sounds, on-screen assets, behaviors, and sets of film clips (called 'branches') that play when transferring them to other rooms. A room usually corresponds to a single physical region (a 'room') in a game map, but could be any interactive montage that makes conceptual sense.  
--- web/js/Inventory.js
+++ web/js/Inventory.js
@@ -8,23 +8,23 @@ class InventoryItem {
     // 'exit' and 'use' icons:
     const xImage = document.createElement('img');
     xImage.src = '/images/x.jpg';
-    const useImage = document.createElement('img');
-    useImage.src = '/images/use.jpg';
+    this.useImage = document.createElement('img');
+    this.useImage.src = '/images/use.jpg';
     this.item = item;
     this.position = { x: iconsWidth / 2, y: 10, radians: 0.35 };
     this.element = document.createElement('img');
     this.width = 0; this.height = 0;
-    let exitPositionX = this.position.x - 10;
-    let exitPositionY = this.position.y - 5;
-    let usePositionX = this.position.x + this.width - 10;
+    this.exitPositionX = this.position.x - 10;
+    this.exitPositionY = this.position.y - 5;
+    this.usePositionX = this.position.x + this.width - 10;
     this.element.onload = function(res) {
       if (res && res.srcElement) {
         this.position.x -= (this.width / 3);
         this.width = res.srcElement.width;
         this.height = res.srcElement.height; // Math.max(height-10, res.srcElement.height);
-        exitPositionX = this.position.x - 10;
-        exitPositionY = this.position.y - 5;
-        usePositionX = this.position.x + this.width - 10;
+        this.exitPositionX = this.position.x - 10;
+        this.exitPositionY = this.position.y - 5;
+        this.usePositionX = this.position.x + this.width - 10;
       }
     };
     this.element.src = item.img;
@@ -36,9 +36,9 @@ class InventoryItem {
     context.drawImage(this.element, this.position.x, this.position.y, this.width, this.height);
     if (!this.selected) {
       // draw the x-out button:
-      context.drawImage(xImage, exitPositionX, exitPositionY, iconsWidth, iconsHeight);
+      context.drawImage(xImage, this.exitPositionX, this.exitPositionY, iconsWidth, iconsHeight);
       // draw the use button:
-      context.drawImage(useImage, usePositionX, exitPositionY, iconsWidth, iconsHeight);
+      context.drawImage(this.useImage, this.usePositionX, this.exitPositionY, iconsWidth, iconsHeight);
     }
     context.restore();
   }
@@ -47,22 +47,22 @@ class InventoryItem {
     return ((x > this.position.x) && (x < this.position.x + this.width)
     && (y > this.position.y) && y < this.position.y + this.height)
   }
-  hitExit(x,y) {
-    return ((x > exitPositionX) && (x < exitPositionX + iconsWidth)
-    && (y > exitPositionY) && y < exitPositionY + iconsHeight)
+  hitExit(x, y) {
+    return ((x > this.exitPositionX) && (x < this.exitPositionX + iconsWidth)
+    && (y > this.exitPositionY) && y < this.exitPositionY + iconsHeight)
   }
-  hitUse(x,y) {
-    return ((x > usePositionX) && (x < usePositionX + iconsWidth)
-    && (y > exitPositionY) && y < exitPositionY + iconsHeight)
+  hitUse(x, y) {
+    return ((x > this.usePositionX) && (x < this.usePositionX + iconsWidth)
+    && (y > this.exitPositionY) && y < this.exitPositionY + iconsHeight)
   }
-  handleClick(x,y) {
+  handleClick(x, y) {
     if (this.hitExit(x, y)) {
       return { exit: true } ;
     }
-    else if (this.hitUse(x,y)) {
+    else if (this.hitUse(x, y)) {
       return { use: this };
     }
-    else if (this.hitTest(x,y)) {
+    else if (this.hitTest(x, y)) {
       return { inventory: this.item };
     }
   }
@@ -85,12 +85,12 @@ class InventoryThumbnail{
   draw(context) {
     context.drawImage(this.element,position.x, position.y, this.width, this.height);
   };
-  hitTest(x,y) {
+  hitTest(x, y) {
     return ((x>this.position.x) && (x<this.position.x+this.width)
     && (y>this.position.y) && y<this.position.y+this.height)
   }
-  handleClick(x,y) {
-    if (this.hitTest(x,y))
+  handleClick(x, y) {
+    if (this.hitTest(x, y))
     return {thumbnail: this.item} ;
   }
 }
@@ -107,12 +107,12 @@ class InventoryButton{
   draw(context) {
     context.drawImage(this.element,this.position.x, this.position.y, this.width, this.height);
   }
-  hitTest(x,y) {
+  hitTest(x, y) {
     return ((x>this.position.x) && (x<this.position.x+this.width)
     && (y>this.position.y) && y<this.position.y+this.height)
   }
-  handleClick(x,y) {
-    if (this.hitTest(x,y))
+  handleClick(x, y) {
+    if (this.hitTest(x, y))
     return {inventoryButton: true} ;
   }
 }
