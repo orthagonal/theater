584cbabc7e61395234530c9eb9e90d0769b8439b
orthagonal <orthagonal1@gmail.com>
2017-07-07 22:08:11 -0500

    update to handle gameObjects in core

--- js/CoreController.js
+++ js/CoreController.js
@@ -14,11 +14,14 @@ class CoreController {
     this.audioController = new AudioController($);
     // todo: make module initialization nicer:
     const Module = require(modulePath);
+    this.gameObjects = {};
     this.module = new Module(this);
+    this.activeObject = null;
   }
 
   // modules will take over the video controller here
   kickstart(gameId, userId) {
+    this.module.controller = this;
     this.module.startNewGame(userId);
   }
 }
