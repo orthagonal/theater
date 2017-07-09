0de13ff367bd4fe3e2f9b707d732cdc1fed2b0b3
orthagonal <orthagonal1@gmail.com>
2017-07-09 00:25:28 -0500

    reorg

--- js/MainSwitcher.js
+++ js/MainSwitcher.js
@@ -1,8 +1,10 @@
 'use strict';
-const path = require('path');
+
 class MainSwitcher {
   constructor(videoContext) {
     this.mainNode = videoContext.transition(videoContext.DEFINITIONS.CROSSFADE);
+    this.mainNode.name = 'MainSwitcher.mainNode';
+    videoContext.destination.name = "DestinationNode";
     // this.mainNode.transition(2,4,0.0,1.0, "mix");
     this.mainNode.connect(videoContext.destination);
     this.videoContext = videoContext;
@@ -11,23 +13,19 @@ class MainSwitcher {
 
   // plug a src scene switcher into this one:
   connect(intrasceneSwitcher) {
-    intrasceneSwitcher.transitionNode.connect(this.mainNode);
+    intrasceneSwitcher.effectNode.connect(this.mainNode);
   }
 
-  // // start a scene without hesitation:
-  // start(sceneNodes) {
-  //   sceneNodes.sceneSwitchingNode.connect(this.mainNode);
-  //   sceneNodes.play();
-  // }
   // transition to a scene:
   transitionToScene(sceneNodes, transitionType) {
+    console.log('transitionToScene is called');
     // attach the new nodes
-    sceneNodes.sceneSwitchingNode.connect(this.mainNode);
+    // sceneNodes.sceneSwitchingNode.connect(this.mainNode);
     // transition to this.mainNode
     this.mainNode.transition(1.0, 1.1, 0.0, 1.0, 'mix');
     // remove the old ones
-    this.currentNodes.sceneSwitchingNode.disconnect(this.mainNode);
-    this.currentNodes = sceneNodes;
+    // this.currentNodes.sceneSwitchingNode.disconnect(this.mainNode);
+    // this.currentNodes = sceneNodes;
   }
 }
 
