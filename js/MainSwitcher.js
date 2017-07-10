1b8e9791af6440561d6057f7212b0aadb2ed0f62
orthagonal <orthagonal1@gmail.com>
2017-07-09 22:52:49 -0500

    root node should be compositor

--- js/MainSwitcher.js
+++ js/MainSwitcher.js
@@ -2,10 +2,9 @@
 
 class MainSwitcher {
   constructor(videoContext) {
-    this.mainNode = videoContext.transition(videoContext.DEFINITIONS.CROSSFADE);
+    this.mainNode = videoContext.compositor(videoContext.DEFINITIONS.COMBINE);
     this.mainNode.name = 'MainSwitcher.mainNode';
-    videoContext.destination.name = "DestinationNode";
-    // this.mainNode.transition(2,4,0.0,1.0, "mix");
+    videoContext.destination.name = 'DestinationNode';
     this.mainNode.connect(videoContext.destination);
     this.videoContext = videoContext;
     this.currentNodes = undefined;
@@ -14,18 +13,6 @@ class MainSwitcher {
   // plug a src scene switcher into this one:
   connect(intrasceneSwitcher) {
     intrasceneSwitcher.effectNode.connect(this.mainNode);
-  }
-
-  // transition to a scene:
-  transitionToScene(sceneNodes, transitionType) {
-    console.log('transitionToScene is called');
-    // attach the new nodes
-    // sceneNodes.sceneSwitchingNode.connect(this.mainNode);
-    // transition to this.mainNode
-    this.mainNode.transition(1.0, 1.1, 0.0, 1.0, 'mix');
-    // remove the old ones
-    // this.currentNodes.sceneSwitchingNode.disconnect(this.mainNode);
-    // this.currentNodes = sceneNodes;
   }
 }
 
