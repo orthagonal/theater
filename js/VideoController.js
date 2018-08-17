a0b653de0d325bbacc5f8cddf7a4ec0fcaaf1c46
orthagonal <orthagonal1@gmail.com>
2018-08-17 00:24:41 -0500

    support syncswitch behavior

--- js/VideoController.js
+++ js/VideoController.js
@@ -12,7 +12,9 @@ class VideoController extends EventEmitter {
     this.controller = controller;
     this.theWindow = theWindow;
     this.dimensions = controller.dimensions;
-    this.currentVideo = false;
+    this.currentVideo = false; // the main video
+    this.backgroundVideo = false; // the secondary video
+    this.loopVideo = false; // the loop video
     this.lastVideo = false;
     this.nextVideo = false;
     this.videoFetcher = false;
@@ -24,14 +26,14 @@ class VideoController extends EventEmitter {
     this.switcher.goUp = val;
   }
 
-  // called when previous video is finished playing:
+  // called when previous video is finished playing, continuously:
   previousEnd() {
     if (this.branching) {
       return this.branchEnd();
     }
     // delete this.currentVideo.onended;
     this.currentVideo = this.activeObject.getNextVideo();
-    this.switcher.connectVideo(this.currentVideo.element);
+    this.switcher.connectVideo(this.currentVideo.element, true);
     if (this.currentVideo.hasMask) {
       // play mask video for interface controller and as input to the switcher:
       this.switcher.connectMask(this.controller.interfaceController.connectMask(this.currentVideo.maskPath));
@@ -43,7 +45,7 @@ class VideoController extends EventEmitter {
   branchEnd() {
     // insert the branch
     this.currentVideo = this.branching.sourceVideo;
-    this.switcher.connectVideo(this.currentVideo.element);
+    this.switcher.connectVideo(this.currentVideo.element, true);
     const wrapper = () => {
       this.branching.destinationObject.activate(this);
       this.branching = false;
@@ -65,7 +67,7 @@ class VideoController extends EventEmitter {
         this.currentVideo.element.onended = undefined;
       }
       this.currentVideo = sourceVideo;
-      this.switcher.connectVideo(this.currentVideo.element);
+      this.switcher.connectVideo(this.currentVideo.element, true);
       this.currentVideo.element.play();
       return;
     }
@@ -77,15 +79,60 @@ class VideoController extends EventEmitter {
     }
   }
 
-  // Active object calls this:
-  startScene(scene) {
+  previousEndSyncSwitch() {
+    const temp = this.backgroundVideo;
+    this.backgroundVideo = this.currentVideo;
+    this.currentVideo = temp;
+    this.switcher.connectVideo(this.currentVideo.element);
+    this.currentVideo.muted = 'true';
+    this.currentVideo.element.play();
+    this.backgroundVideo.element.play();
+    // if (this.currentVideo.hasMask) {
+    //   this.switcher.connectMask(this.controller.interfaceController.connectMask(this.currentVideo.maskPath));
+    // }
+    setTimeout(() => {
+      this.previousEndSyncSwitch.bind(this)();
+    }, 3000);//Math.max(this.sceneDescription.behavior.minCutLength, Math.floor(Math.random() * Math.floor(scene.behavior.maxCutLength))));
+  }
+
+  startSyncSwitch(scene) {
+    // remove previous element.eventHandler
     if (this.currentVideo) {
       this.currentVideo.element.onended = undefined;
     }
-    // todo: might need to remove previous element.eventHandler
+    if (this.backgroundVideo) {
+      this.backgroundVideo.element.onended = undefined;
+    }
+    this.sceneDescription = scene;
+    // start first root and first loop
+    this.currentVideo = scene.roots[0];
+    this.backgroundVideo = scene.loops[0];
+    this.switcher.connectVideo(this.currentVideo.element);
+    // this.currentVideo.element.onended = this.previousEnd.bind(this);
+    this.currentVideo.element.play();
+    this.backgroundVideo.element.play();
+    // set a timeout to switch to the next one of random length
+    // between the min and max:
+    setTimeout(() => {
+      this.previousEndSyncSwitch.bind(this)();
+    }, 2000);// Math.max(scene.behavior.minCutLength, Math.floor(Math.random() * Math.floor(scene.behavior.maxCutLength))));
+    if (this.currentVideo.hasMask) {
+      this.switcher.connectMask(this.controller.interfaceController.connectMask(this.currentVideo.maskPath));
+    }
+  }
+
+  // Active object calls this:
+  startScene(scene) {
+    if (scene.behavior.behavior_type === 'syncSwitch') {
+      return this.startSyncSwitch(scene);
+    }
+    // remove previous element.eventHandler
+    if (this.currentVideo) {
+      this.currentVideo.element.onended = undefined;
+    }
     this.sceneDescription = scene;
     this.currentVideo = this.activeObject.getNextVideo();
-    this.switcher.connectVideo(this.currentVideo.element);
+    this.switcher.connectVideo(this.currentVideo.element, true);
     this.currentVideo.element.onended = this.previousEnd.bind(this);
     this.currentVideo.element.play();
     if (this.currentVideo.hasMask) {
